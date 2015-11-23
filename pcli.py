db3d76ccde3823c2c7d0ef73e9f5ac1b10c6c2a8
Adam Otto <aotto@pclhcb194.cern.ch>
2015-11-23 14:09:35 +0100

    Added logging.

--- pcli.py
+++ pcli.py
@@ -4,7 +4,7 @@ Created on Nov 9, 2015
 @author: aotto
 '''
 
-import pexpect, re
+import pexpect, logging
 import argparse
 import getpass
 import sys, os
@@ -26,7 +26,7 @@ def switch_connect(hostname, user, password):
             child.sendline(password)
             try:
                 child.expect_exact('Permission denied, please try again.',0.2)
-                print "\nWrong password."
+                logging.error("\n%s@%s: Wrong password."%(user,hostname))
                 close_connection(child)
                 gracefully_exit()
             except: 
@@ -34,12 +34,12 @@ def switch_connect(hostname, user, password):
             #child.readline()
             enable = child.expect('%s#'%hostname)
             if enable == 0:
-                print "Succesfully logged."
+               logging.debug("\n%s@%s: Succesfully logged."%(user,hostname))
         elif err==1:
-            print "Something went wrong (unknown host?) or connection timed out.\n"
+            logging.error("\n%s@%s:Something went wrong (unknown host?) or connection timed out.\n"%(user,hostname))
             pass
     except Exception as e:
-        print e
+        logging.exception("\n%s@%s: Please check this error:"%(user,hostname))
         exit(0)
     return child
         
@@ -55,7 +55,7 @@ def command_to_send(command,datalist=None, iteration=1):
          
      return commands
      
-def send_command(connectionid, commands):
+def send_command(connectionid, commands,hostname='',user=''):
     for command in commands:
         connectionid.sendline(command)
         err = connectionid.expect([prompt,more, error])
@@ -65,12 +65,13 @@ def send_command(connectionid, commands):
                 connectionid.send(linesep)
                 err = connectionid.expect([prompt,more, error])
         if err==2:
-            print "Incorrect command. Probably unsupported by switch OS.\n"
+            logging.error("\n%s@%s: Incorrect command. Probably unsupported by switch OS."%(hostname,user))
             continue
         if err==0:
             continue
 
-def close_connection(connectionid):
+def close_connection(connectionid,user='',hostname=''):
+        logging.debug("%s@%s: Closing connection."%(user,hostname))
         connectionid.close()
         
 def is_file(parser, arg):
@@ -96,8 +97,9 @@ def unnest(nested_list):
     return single_list
 
 def get_argument():
-    parser = argparse.ArgumentParser(description="Execute command on multiple switches.", formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description="Execute command on multiple switches.", formatter_class=argparse.RawTextHelpFormatter,epilog="Current version(0.1) assumes one password for all used devices.")
     parser.add_argument('-u','--user',help='User to use in order to log into switches. By default admin.',default='admin')
+    parser.add_argument('-v','--verbose',help='Turn on verbose.',action='store_true')
     parser.add_argument('-c','--command',nargs='+',required=True,help='[REQUIRED] Command to perform on the switches.\
 Commands can be read from the file (preffered)or from command line example: -c "show clock" "show interfaces" "etc ..".\nFile should contain all the commands that you would type on the switch to obtain desired output. Example:\n\
     conf t\n\
@@ -110,25 +112,36 @@ Commands can be read from the file (preffered)or from command line example: -c "
     switch = args.switch
     user = args.user
     command = args.command
+    debug = args.verbose
     if is_nested(switch):
-        return unnest(switch), user, command           
-    return switch, user, command
+        return unnest(switch), user, command, debug         
+    return switch, user, command, debug
 
+def debug_logging(debug_on):
+    #FORMAT="%(user)s@%(hostname)s: %(message)s"
+    debug_lvl=logging.ERROR
+    if debug_on:
+        debug_lvl=logging.DEBUG
+        
+    FORMAT="%(message)s"
+    logging.basicConfig(format=FORMAT, level=debug_lvl)
+    
+    
 
 if __name__ == '__main__':
     
+    switch, user, command, debug = get_argument()
     
-    #user=''
-    switch, user, command = get_argument()
-    print switch, user
     command = command_to_send(command)
-    print command
+    #exit(1)
+    debug_logging(debug)
+    logging.debug("User: %s\nSwitches: %s\nCommands: %s\nDebug: %s\n"%(user,switch,command,debug))
     #exit(1)
     passwd = getpass.getpass('Please provide password for user %s:'%user)
-
+    
     for host in switch:
         connection = switch_connect(host, user, passwd)
-        send_command(connection, command)
-        close_connection(connection)
+        send_command(connection, command,user,host)
+        close_connection(connection,user,host)
     
     gracefully_exit()
